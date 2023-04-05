e4b7407683e66346014d3db04a249c4dc0034ebc
Alex <moldovanalexismihai06@gmail.com>
2023-04-05 20:32:00 +0300

    post controller done

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,20 +1,75 @@
-const getAllPosts = async(req,res)=>{
-    res.send('allposts')
+const Post = require('../models/Post')
+const BadRequest = require('../errors/BadRequest')
+const NotFound = require('../errors/NotFound')
+
+const getAllPosts = async(req,res,next)=>{
+    try{
+        const post = await Post.find({}).sort('createdAt')
+        res.status(200).json({post,count:post.length})
+    }catch(error){
+        next(error)
+    }
 }
-const createPost = async(req,res)=>{
-    res.send('created')
+const createPost = async(req,res,next)=>{
+    try{
+        req.body.createdBy = req.user.userId
+        const post = await Post.create(req.body)
+        res.status(201).json({post})
+    }catch(error){
+        next(error)
+    }
 }
 
-const getPost = async(req,res)=>{
-    res.send('single post')
+const getPost = async(req,res,next)=>{
+    try{
+        const {user:{userId},params:{id:postId}} = req
+        const post = await Post.findOne({_id:postId,createdBy:userId})
+        if(!post)
+            throw new NotFound(`No post with id ${postId}`) 
+
+        res.status(200).json({post})
+    }
+    catch(error){
+        next(error)
+    }
+   
 }
 
-const updatePost =  async(req,res)=>{
-    res.send('updated post')
+const updatePost =  async(req,res,next)=>{
+    try{
+        const {body:{description,imageUrl},user:{userId},params:{id:postId}} = req
+
+        if(description.trim()==='' || !imageUrl)
+            throw new BadRequest('You must fill all the inputs')
+    
+        const post = await Post.findByIdAndUpdate({_id:postId,createdBy:userId},req.body,{new:true,runValidators:true})
+    
+        if(!post)
+            throw new NotFound(`No job with the id ${postId}`)
+    
+        res.status(200).json({post})
+    }
+    catch(error)
+    {
+        next(error)
+    }
+    
 }
 
-const deletePost = async(req,res)=>{
-    res.send('deleted post')
+const deletePost = async(req,res,next)=>{
+    try{
+        const {user:{userId},params:{id:postId}} = req
+        const post = await Post.findOneAndRemove({_id:postId,createdBy:userId})
+        
+        if(!post)
+            throw new NotFound(`No post with id ${postId}`)
+
+        res.status(200).json({post})
+    }
+    catch(error){
+        next(error)
+    }
+
 }
 
 
---
+++ models/Post.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const PostSchema = new mongoose.Schema({
+    description:{
+        type:String,
+        maxlength:150
+    },
+    imageUrl:{
+        type:String,
+        required:[true,'Please provide an image URL in order to continue']
+    },
+    createdBy:{
+        type:mongoose.Types.ObjectId,
+        ref:'User'
+    }
+},{timestamps:true})
+
+module.exports = mongoose.model('Post',PostSchema)
