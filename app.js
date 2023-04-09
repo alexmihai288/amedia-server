8d521bafffd1ed82bd3e605f527760748e1ad640
Alex <moldovanalexismihai06@gmail.com>
2023-04-09 21:02:00 +0300

    multiple things

--- app.js
+++ app.js
@@ -27,10 +27,7 @@ const errorHandler  = require('./middlewares/error-handler')
 app.use(express.json());
 
 app.set('trust proxy',1)
-app.use(rateLimit({
-    windowMs: 15 * 60 * 1000,// 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
- }))
+
  //protection packages 
  app.use(helmet())
  app.use(cors())
